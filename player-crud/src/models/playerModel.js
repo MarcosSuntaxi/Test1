25bfd1540a80bdfa4e2c59a3f1b64816f921e979
MarcosSuntaxi <masuntaxic@uce.edu.ec>
2024-12-16 17:13:22 -0500

    feat: :sparkles: Update text

--- player-crud/src/models/playerModel.js
+++ player-crud/src/models/playerModel.js
@@ -1,31 +1,27 @@
 const db = require('../config/db');
 
-const Player = {
-  getAll: (callback) => {
-    db.query('SELECT * FROM players', callback);
+// Funciones para interactuar con la base de datos
+const PlayerModel = {
+  getAll: async () => {
+    const [rows] = await db.query('SELECT * FROM players');
+    return rows;
   },
-  getById: (id, callback) => {
-    db.query('SELECT * FROM players WHERE id = ?', [id], callback);
+  getById: async (id) => {
+    const [rows] = await db.query('SELECT * FROM players WHERE id = ?', [id]);
+    return rows[0];
   },
-  create: (player, callback) => {
+  create: async (player) => {
     const { image, name, country, team } = player;
-    db.query(
-      'INSERT INTO players (image, name, country, team) VALUES (?, ?, ?, ?)',
-      [image, name, country, team],
-      callback
-    );
+    const [result] = await db.query('INSERT INTO players (image, name, country, team) VALUES (?, ?, ?, ?)', [image, name, country, team]);
+    return result.insertId;
   },
-  update: (id, player, callback) => {
+  update: async (id, player) => {
     const { image, name, country, team } = player;
-    db.query(
-      'UPDATE players SET image = ?, name = ?, country = ?, team = ? WHERE id = ?',
-      [image, name, country, team, id],
-      callback
-    );
+    await db.query('UPDATE players SET image = ?, name = ?, country = ?, team = ? WHERE id = ?', [image, name, country, team, id]);
   },
-  delete: (id, callback) => {
-    db.query('DELETE FROM players WHERE id = ?', [id], callback);
+  delete: async (id) => {
+    await db.query('DELETE FROM players WHERE id = ?', [id]);
   },
 };
 
-module.exports = Player;
+module.exports = PlayerModel;
